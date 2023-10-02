0c3e8150d1322ca6632eb8c2d177d865c67084b5
Arun R Nair <arnair0899@gmail.com>
2023-10-02 11:10:58 +0530

    added

---
+++ ecommerceproject/shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+app_name='shop'
+urlpatterns = [
+    path('',views.allproduct,name='allproduct'),
+    path('<slug:c_slug>/',views.allproduct,name='products_by_category'),
+    path('<slug:c_slug>/<slug:Product_slug>/', views.proDetail, name='procatdetail'),
+
+]
